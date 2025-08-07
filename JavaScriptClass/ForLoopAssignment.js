394110709c95fdd36980c3a413bbc918808a142d
Devesh-Shisode <deveshshisode@gmail.com>
2025-08-07 23:18:42 +0530

    for loop statetement assignment

--- JavaScriptClass/ForLoopAssignment.js
+++ JavaScriptClass/ForLoopAssignment.js
@@ -2,12 +2,13 @@
 for (let i = 5; i <= 15; i++) {
   console.log(i);
 }
+console.log("----------------------------------------------------");
 
  
 for (let i = 50; i >= 40; i--) {
   console.log(i);
 }
-
+console.log("----------------------------------------------------");
  
 let countOdd = 0;
 for (let i = 1; countOdd < 15; i += 2) {
@@ -15,24 +16,25 @@ for (let i = 1; countOdd < 15; i += 2) {
   countOdd++;
 }
 
- 
+ console.log("----------------------------------------------------");
 let countEven = 0;
 for (let i = 0; countEven < 10; i += 2) {
   console.log(i);
   countEven++;
 }
-
+console.log("----------------------------------------------------");
  
 for (let i = 5; i <= 50; i += 5) {
   console.log(i);
 }
-
+console.log("----------------------------------------------------");
  
 for (let i = 10; i <= 100; i += 10) {
   console.log(i);
 }
-
+console.log("----------------------------------------------------");
  
 for (let i = 100; i >= 10; i -= 10) {
   console.log(i);
 }
+console.log("----------------------------------------------------");
